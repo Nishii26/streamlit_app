4bb11ae80d0ef1bbd2eedb5bc1d133bff42e8b79
Nishii26 <gnishtha01@gmail.com>
2023-08-08 15:07:12 -0400

    Update streamlit_ap.py

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -30,6 +30,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list( 'from streamlit')")
+
